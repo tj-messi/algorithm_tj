825a2283fddadde73348742cc32559c86c19a8c8
tj-messi <2715978522@qq.com>
2024-08-30 23:38:30 +0800

    1

--- cf/round969-div2/2007C.cpp
+++ cf/round969-div2/2007C.cpp
@@ -1,13 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std; 
+#define ll long long
 #define int long long
 #define ull unsigned long long
 #define endl '\n' 
 
+const int N = 1e5+5;
+int arr[N];
 
 void solve()
 {
-
+	int n, a, b;
+    cin >> n >> a >> b;
+    int c = __gcd(a, b);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> arr[i];
+        arr[i] %= c;
+    }
+    sort(arr+1, arr+1+n);
+    int max_v= arr[n];
+    ll res = max_v - arr[1];
+    for (int i = 1; i < n; ++i)
+    {
+        res = min(res, (ll)arr[i] + c - arr[i + 1]);
+    }
+    cout << res << endl;
 }
 
 signed main()
