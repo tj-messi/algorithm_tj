181da916f1898e8c145256390512e3845423ffc0
tj-messi <2715978522@qq.com>
2024-09-20 23:20:32 +0800

    1

--- cf/round973-div2/2011C.cpp
+++ cf/round973-div2/2011C.cpp
@@ -4,15 +4,56 @@ using namespace std;
 #define ull unsigned long long
 #define endl '\n' 
 
+int qry(string s)
+{
+	cout<<"? "<<s<<endl;
+	int res;cin>>res;
+	return res;
+}
+
 
 void solve()
 {
-	
+	int n;cin>>n;
+	string now="";
+	bool flag=false;
+	while(n--)
+	{
+		if(!flag)
+		{
+			if(qry(now+"0"))//now+0ÊÇ 
+			{
+				now=now+"0";continue;
+			}
+			else if(qry(now+"1"))//now+1ÊÇ 
+			{
+				now=now+"1";continue;
+			}
+			else
+			{
+				n++;
+				flag=true;
+				continue;
+			}
+		}
+		else
+		{
+			if(qry("0"+now))
+			{
+				now="0"+now;continue;
+			}
+			else
+			{
+				now="1"+now;continue;
+			}
+		}
+	}
+	cout<<"! "<<now<<endl;
 }
 
 signed main()
 {
-	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	//ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 	int t;
 	cin>>t;
 	//t=1;
--- cf/round973-div2/2011D.cpp
+++ cf/round973-div2/2011D.cpp
@@ -4,10 +4,56 @@ using namespace std;
 #define ull unsigned long long
 #define endl '\n' 
 
+int qry(string s)
+{
+	cout<<"? "<<s<<endl;
+	int res;cin>>res;
+	return res;
+}
+
+	
 
 void solve()
 {
-	
+	int n;cin>>n;
+	string now="";
+	bool flag=0;
+	while(n--)
+	{
+		if(flag)
+		{
+			if(qry(now+"0"))//now+0ÊÇ 
+			{
+				now=now+"0";
+			}
+			else if(qry(now+"1"))//now+1ÊÇ 
+			{
+				now=now+"1";
+			}
+			else
+			{
+				n++;
+				flag=true;
+			}
+		}
+		else
+		{
+			if(qry("0"+now))
+			{
+				now="0"+now;
+			}
+			else if(qry("1"+now))
+			{
+				now="1"+now;
+			}
+			else
+			{
+				n++;
+				flag=false;
+			}
+		}
+	}
+	cout<<"! "<<now<<endl;
 }
 
 signed main()
