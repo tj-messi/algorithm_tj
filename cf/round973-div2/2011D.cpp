cdcc2b11e49ee64ccac1590db16bc0c22d40eae5
tj-messi <2715978522@qq.com>
2024-09-21 00:32:27 +0800

    1

--- cf/round973-div2/2011D.cpp
+++ cf/round973-div2/2011D.cpp
@@ -4,56 +4,118 @@ using namespace std;
 #define ull unsigned long long
 #define endl '\n' 
 
-int qry(string s)
+const int N = 2e5+5;
+int a[N];
+int n;
+bool check(int num)
 {
-	cout<<"? "<<s<<endl;
-	int res;cin>>res;
-	return res;
-}
-
-	
-
-void solve()
-{
-	int n;cin>>n;
-	string now="";
-	bool flag=0;
-	while(n--)
+	int temp=0;
+	for(int i=1;i<=n;i++)
 	{
-		if(flag)
+		if(a[i]>=num)
 		{
-			if(qry(now+"0"))//now+0ÊÇ 
-			{
-				now=now+"0";
-			}
-			else if(qry(now+"1"))//now+1ÊÇ 
-			{
-				now=now+"1";
-			}
-			else
-			{
-				n++;
-				flag=true;
-			}
+			temp+=a[i]-num;
 		}
 		else
 		{
-			if(qry("0"+now))
+			if(temp>0)
 			{
-				now="0"+now;
-			}
-			else if(qry("1"+now))
-			{
-				now="1"+now;
-			}
-			else
-			{
-				n++;
-				flag=false;
+				temp=max(0ll,temp+a[i]-num);
 			}
 		}
 	}
-	cout<<"! "<<now<<endl;
+	if(temp==0)
+		return 1;
+	else
+		return 0;
+}
+
+bool check0(int num)
+{
+	int rem = 0;
+	for (int i = n ; i >= 1; --i)
+	{
+		if (a[i] <= num)
+		{
+			rem += num - a[i];
+		}
+		else
+		{
+			rem = max(0ll, rem - (a[i] - num));
+		}
+	}
+	if (rem == 0)
+		return 1;
+	else
+		return 0;
+}
+
+
+void solve()
+{
+	cin>>n;
+	int max_num=0;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>a[i];
+		max_num=max(max_num,a[i]);
+	}
+	int l=0,r=max_num;
+	int min_maxnum=max_num;
+	while(l<=r)
+	{
+		int mid=(l+r)/2;
+		if(check(mid))
+		{
+			r=mid-1;
+			min_maxnum=min(min_maxnum,mid);
+		}
+		else
+		{
+			l=mid+1;
+		}
+	}
+	int top=min_maxnum;
+	int temp=0;
+	for(int i=1;i<=n;i++)
+	{
+		if(a[i]>=top)
+		{
+			temp+=a[i]-top;
+			a[i]=top;
+		}
+		else
+		{
+			if(temp>=(top-a[i]))
+			{
+				temp-=(top-a[i]);
+				a[i]=top;
+			}
+			else
+			{
+				a[i]+=temp;temp=0;
+			}
+			
+		}
+	}
+	l=0;r=top;
+	int max_minnum=0;
+	while(l<=r)
+	{
+		int mid=(l+r)/2;
+		if(check0(mid))
+		{
+			l=mid+1;
+			max_minnum=max(max_minnum,mid);
+		}
+		else
+		{
+			r=mid-1;
+		}
+	}
+	int down=max_minnum;
+	//cout<<top<<" "<<down<<endl;
+	cout<<top-down<<endl;
 }
 
 signed main()
