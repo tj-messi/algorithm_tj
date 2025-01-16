1fd69d20fb97a08377961e9cf07896accdc29bbd
tj-messi <2715978522@qq.com>
2025-01-16 12:31:12 +0800

    1

--- 蓝桥杯/基础算法-依依的画作.cpp
+++ 蓝桥杯/基础算法-依依的画作.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 #define int long long
 #define endl '\n'
-
-const int N = 1e5+5;
-int a[N];
+                                        
 
 void solve()
 {
 	int n;cin>>n;
-	for(int i=1;i<=n;i++)cin>>a[i];
 	int ans=0;
-	for(int i=2;i<=n-1;i++)
+	vector<string> v(n+2);
+	for(int i = 1;i<=n;i++)
 	{
-		if(a[i]>=a[i-1]+a[i+1])ans++;
-	}
+		cin>>v[i]; 
+		if(i!=1)
+		{
+			if(v[i][0]!=v[i-1][1])ans++;
+		}
+	} 
 	cout<<ans<<endl;
 }
+
 
 signed main()
 {
